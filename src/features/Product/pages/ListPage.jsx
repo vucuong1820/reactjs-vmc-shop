3aa0109973540d5227af0f7408b92fc93032a779
Manh Cuong <vucuong1820@gmail.com>
2022-02-19 17:29:15 +0700

    Using skeleton for loading product
    - While fetching product api , use skeleton for loading product list

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 import productApi from '../../../api/productApi';
+import ProductSkeleton from '../components/ProductSkeleton';
 
 ListPage.propTypes = {};
 const useStyles = makeStyles((theme) => ({
@@ -17,10 +18,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 function ListPage(props) {
   const classes = useStyles();
+  const [productList, setProductList] = useState([]);
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     (async () => {
       const response = await productApi.getAll({ _page: 1, _limit: 10 });
       console.log('response: ', response);
+
+    //   setLoading(false)
     })();
   }, []);
   return (
@@ -32,7 +37,11 @@ function ListPage(props) {
           </Grid>
 
           <Grid item className={classes.right}>
-            <Paper elevation={0}>Right</Paper>
+            <Paper elevation={0}>
+                {
+                    loading ? <ProductSkeleton/> : <Typography>Product List</Typography>
+                }
+            </Paper>
           </Grid>
         </Grid>
       </Container>
