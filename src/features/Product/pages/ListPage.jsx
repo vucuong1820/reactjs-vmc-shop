1f667e611a06ea2e7e0232b5d5c1aa14477c5fe7
Manh Cuong <vucuong1820@gmail.com>
2022-02-24 19:44:02 +0700

    Support Back click on browser
    - Re-render product list if click  back/next browser
    - Instead of using state, using query params

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,14 +1,14 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import productApi from '../../../api/productApi';
 import ProductList from '../components/ProductList';
 import ProductSkeleton from '../components/ProductSkeleton';
 import ProductSort from '../components/ProductSort';
-import ProductFilters from '../components/ProductFilters'
+import ProductFilters from '../components/ProductFilters';
 import FilterViewer from '../components/Filters/FilterViewer';
 import { useHistory, useLocation } from 'react-router-dom';
-import queryString from 'query-string'
+import queryString from 'query-string';
 
 ListPage.propTypes = {};
 const useStyles = makeStyles((theme) => ({
@@ -25,13 +25,23 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     marginTop: '20px',
-    paddingBottom: '20px'
-  }
+    paddingBottom: '20px',
+  },
 }));
 function ListPage(props) {
   const history = useHistory();
   const location = useLocation();
-  const queryParams = queryString.parse(location.search);
+  const queryParams = useMemo(() => {
+    const params = queryString.parse(location.search);
+    return {
+      ...params,
+      _page: Number.parseInt(params._page) || 1,
+      _limit: Number.parseInt(params._limit) || 9,
+      _sort: params._sort || 'salePrice:ASC',
+      isPromotion: params.isPromotion === 'true',
+      isFreeShip: params.isFreeShip === 'true',
+    };
+  }, [location.search]);
 
   const classes = useStyles();
   const [productList, setProductList] = useState([]);
@@ -47,25 +57,24 @@ function ListPage(props) {
   //   _sort: 'salePrice:ASC'
   // });
 
-  const [filters, setFilters] = useState(() => ({
-    ...queryParams,
-    _page: Number.parseInt(queryParams._page) || 1,
-    _limit: Number.parseInt(queryParams._limit) || 9,
-    _sort: queryParams._sort || 'salePrice:ASC'
-  }));
+  // const [filters, setFilters] = useState(() => ({
+  //   ...queryParams,
+  //   _page: Number.parseInt(queryParams._page) || 1,
+  //   _limit: Number.parseInt(queryParams._limit) || 9,
+  //   _sort: queryParams._sort || 'salePrice:ASC'
+  // }));
 
-  useEffect(() => {
-    history.push({
-      pathname: history.location.pathname,
-      search: queryString.stringify(filters)
-    })
-  },[history, filters])
-
+  // useEffect(() => {
+  //   history.push({
+  //     pathname: history.location.pathname,
+  //     search: queryString.stringify(filters)
+  //   })
+  // },[history, filters])
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await productApi.getAll(filters);
+        const response = await productApi.getAll(queryParams);
         setProductList(response.data);
         setPagination(response.pagination);
 
@@ -74,49 +83,75 @@ function ListPage(props) {
         console.log('Failed to fetch product api:', error);
       }
     })();
-  }, [filters]);
+  }, [queryParams]);
   const handlePageChange = (e, page) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
+    // setFilters((prevFilters) => ({
+    //   ...prevFilters,
+    //   _page: page,
+    // }));
+    const filters = {
+      ...queryParams,
       _page: page,
-    }));
+    };
+    history.push({
+      pathname: history.location.pathname,
+      search: queryString.stringify(filters),
+    });
   };
   const handleSortChange = (e, newValue) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
+    // setFilters((prevFilters) => ({
+    //   ...prevFilters,
+    //   _sort: newValue,
+    // }));
+    const filters = {
+      ...queryParams,
       _sort: newValue,
-    }));
+    };
+    history.push({
+      pathname: history.location.pathname,
+      search: queryString.stringify(filters),
+    });
   };
 
   const handleFiltersChange = (newFilters) => {
-    setFilters(prevFilters => ({
-      ...prevFilters,
-      ...newFilters
-    }))
-    console.log('filters: ',filters);
+    // setFilters(prevFilters => ({
+    //   ...prevFilters,
+    //   ...newFilters
+    // }))
+    const filters = {
+      ...queryParams,
+      ...newFilters,
+    };
+    history.push({
+      pathname: history.location.pathname,
+      search: queryString.stringify(filters),
+    });
   };
 
   const setNewFilters = (newFilters) => {
-    setFilters(newFilters)
-  }
+    history.push({
+      pathname: history.location.pathname,
+      search: queryString.stringify(newFilters),
+    });
+  };
   return (
     <Box>
       <Container>
         <Grid container spacing={1}>
           <Grid item className={classes.left}>
             <Paper elevation={0}>
-              <ProductFilters onChange={handleFiltersChange} filters={filters}/>
+              <ProductFilters onChange={handleFiltersChange} filters={queryParams} />
             </Paper>
           </Grid>
           <Grid item className={classes.right}>
             <Paper elevation={0}>
-              <ProductSort currentSort={filters._sort} onChange={handleSortChange}/>
-              <FilterViewer filters={filters} onChange={setNewFilters}/>
+              <ProductSort currentSort={queryParams._sort} onChange={handleSortChange} />
+              <FilterViewer filters={queryParams} onChange={setNewFilters} />
               {loading ? <ProductSkeleton length={9} /> : <ProductList data={productList} />}
               <Box className={classes.pagination}>
                 <Pagination
                   count={Math.ceil(pagination.total / pagination.limit)}
-                  page={filters._page}
+                  page={queryParams._page}
                   color="primary"
                   onChange={handlePageChange}
                 />
