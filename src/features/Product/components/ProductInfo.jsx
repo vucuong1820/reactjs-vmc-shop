efb179cc5601f6bc8adffe8de15e56c9b18739ea
Manh Cuong <vucuong1820@gmail.com>
2022-02-26 21:13:10 +0700

    Create quantity field for Product Info

--- src/features/Product/components/ProductInfo.jsx
+++ src/features/Product/components/ProductInfo.jsx
@@ -9,10 +9,12 @@ ProductInfo.propTypes = {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-      paddingBottom: theme.spacing(2),
-      borderBottom: `1px solid ${theme.palette.grey[200]}`
+    padding: theme.spacing(10, 0, 2, 0),
+    borderBottom: `1px solid ${theme.palette.grey[200]}`,
   },
-
+  name: {
+    textTransform: 'uppercase'
+  },
   description: {
     margin: theme.spacing(2, 0),
   },
@@ -36,17 +38,19 @@ function ProductInfo({ product }) {
   const { description, shortDescription, name, originalPrice, salePrice, promotionPercent } = product;
   return (
     <Box className={classes.root}>
-      <Typography component="h1" variant="h4">
+      <Typography component="h1" variant="h4" className={classes.name}>
         {name}
       </Typography>
 
-      <Typography variant="body2" className={classes.description}>{shortDescription}</Typography>
+      <Typography variant="body2" className={classes.description}>
+        {shortDescription}
+      </Typography>
 
       <Box className={classes.priceBox}>
         <Box component="span" className={classes.salePrice}>
           {formatPrice(salePrice)}
         </Box>
-        {salePrice > 0 && (
+        {promotionPercent > 0 && (
           <>
             <Box component="span" className={classes.originalPrice}>
               {formatPrice(originalPrice)}
